6d57d98db3e5310935f3a212c7d6b9741a85a587
Onorio de Jesus <onorio@catalpa.io>
2025-06-11 17:09:07 +0900

    remove text-muted in footer

--- relaxify/settings.py
+++ relaxify/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-!#y6!osy*uy8wrt^hr^5ga$3x(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['relaxify.pythonanywhere.com']
+ALLOWED_HOSTS = ['relaxify.pythonanywhere.com', 'localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
